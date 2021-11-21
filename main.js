434cce051590b9cc23bb8c420f2924d06d939bac
= <zakaria98alaoui@gmail.com>
2021-11-21 13:00:38 +0100

    add project to repo localy

---
+++ main.js
@@ -0,0 +1,7 @@
+// show and hide ul
+let myList = document.querySelector('header nav ul');
+let navToggle = document.querySelector('.nav-toogle');
+
+navToggle.addEventListener('click', function () {
+
+})
